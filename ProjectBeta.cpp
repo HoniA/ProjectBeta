ab48a489e271e3ad6308bc3c6107f324e5884d17
HoniA <honi.ahmadian@nevada.unr.edu>
2017-03-05 15:13:33 -0800

    Add files via upload
    Main program started
    Agent finds goal through one iteration

--- ProjectBeta.cpp
+++ ProjectBeta.cpp
@@ -7,12 +7,13 @@
 #include <vector>
 #include <assert.h>
 #include<algorithm>
+#include <time.h>
 
 using namespace std;
 
 #define SMALLRAND (double)rand()/RAND_MAX*0.1 //falls between 0 and 0.1
 #define GREED_RAND (double)rand()/RAND_MAX // falls between 0 and 1
-#define MOVE_RAND rand()/RAND_MAX*3 //  integer between 0 and 3 (four actions)
+#define MOVE_RAND (double)rand()/RAND_MAX*3 //  integer between 0 and 3 (four actions)
 
 class grid {
 public:
@@ -20,31 +21,44 @@ public:
 	int ymax;
 	int goalPosX;
 	int goalPosY;
+	int goalState;
 	vector<int> xcoord;
 	vector<int> ycoord;
 	vector<int> states;
 	vector<double> rewardTable;
 
-	void init();
+	void init(int x, int y, int goalx, int goaly);
 	void coordToStates();
 	void createRewardTable();
 };
 
-void grid::init() {
-//start by setting values that don't make sense. they will be specified as true values later
-	xmax = -1;
-	ymax = -1;
-	goalPosX = -1;
-	goalPosY = -1;
+void grid::init(int x, int y, int goalx, int goaly) {
+	//doing a different type of init function than previous assignments for more readable and efficient code
+	//max x and y coordinates and position of goal come in as parameters
+	xmax = x;
+	ymax = y;
+	goalPosX = goalx;
+	goalPosY = goaly;
+	goalState = goalx + goaly*(xmax + 1);
+
+	for (int n = 0; n < xmax; n++)
+	{
+		xcoord.push_back(n);
+	}
+
+	for (int m = 0; m < ymax; m++)
+	{
+		ycoord.push_back(m);
+	}
 }
 
 void grid::coordToStates() {
 	//convert coordinates from two vectors into one state number
 	// go through every row
-	for (int n = 0; n < ymax; n++)
+	for (int n = 0; n < ymax+1; n++)
 	{
 		// go through every column
-		for (int m = 0; m < xmax; m++)
+		for (int m = 0; m < xmax+1; m++)
 		{
 			// create state values
 			states.push_back(n + m*(xmax + 1));
@@ -84,7 +98,7 @@ public:
 	void initQ(vector<int> s);
 	void moveAgent(grid g);
 	void checkBumper(grid g, int x, int y);
-	void updateQ(double action, vector<double> reward);
+	void updateQ(double action, vector<double> reward, double currentState);
 
 };
 
@@ -100,8 +114,8 @@ void agent::placeAgent(vector<int> x, vector<int> y, int xmax, int ymax) {
 	int userx;
 	int usery;
 
-	userx = rand() / RAND_MAX*xmax;
-	usery = rand() / RAND_MAX*ymax;
+	userx = (double)rand() / RAND_MAX*(xmax);
+	usery = (double)rand() / RAND_MAX*(ymax);
 
 	// if statements are redundant because the agent position is now randomized instead of being a user input
 	if (userx < 0)
@@ -162,7 +176,7 @@ void agent::checkBumper(grid g, int x, int y)
 	}
 	else
 	{
-		ypos = g.ymax;
+		ypos = y;
 	}
 }
 
@@ -170,17 +184,24 @@ void agent::moveAgent(grid g)
 {
 	int n = GREED_RAND;
 	//outside of loop because i need this sent as parameter to update function
-	double m;
-
+	int m=0; // default
+	
 	if (n < epsilon)
 	{
 		//random move
-		m = MOVE_RAND;
+		m = MOVE_RAND+0.5;
 	}
 	else
 	{
 		//greedy move
-		m = *max_element(qTable[state].begin(), qTable[state].end());
+		int h = *max_element(qTable[state].begin(), qTable[state].end());
+		for (int j = 0; j < 4; j++)
+		{
+			if (qTable[state][j] = h)
+			{
+				m = j;
+			}
+		}
 	}
 
 	if (m == 0)
@@ -202,30 +223,65 @@ void agent::moveAgent(grid g)
 
 	}
 
-	else if (m == 3)
+	else
 	{
 		//move down
 		ypos--;
-	}
-
+	} 
+	
+	//cout << m;
 	//check for bumpers
 	checkBumper(g, xpos, ypos);
 
-	state = xpos + ypos*(g.xmax + 1);
+	double newState = xpos + ypos*(g.xmax + 1);
 
 	//update the q table 
-	updateQ(m,g.rewardTable);
+	updateQ(m,g.rewardTable, newState);
 	numMoves++;
 }
 
-void agent::updateQ(double action, vector<double> reward) 
+void agent::updateQ(double action, vector<double> reward, double currentState) 
 {
 	//this for previous state and action after said action is taken
-	qTable[state - 1][action] = qTable[state-1][action] + alpha*(reward.at(state)+gamma**max_element(qTable[state].begin(),qTable[state].end()- qTable[state - 1][action]));
+	double maxQ = *max_element(qTable[currentState].begin(), qTable[currentState].end());
+	qTable[state][action] = qTable[state][action] + alpha*(reward.at(currentState)+gamma*maxQ - qTable[state][action]);
+	state = currentState;
 }
 
 int main()
 {
+	srand(time(NULL));
+
+	// initialize variables
+	int xmax = 5;
+	int ymax = 5;
+	int goalx = 2;
+	int goaly = 2;
+	
+	// create grid
+	grid gridWorld;
+	gridWorld.init(xmax, ymax, goalx, goaly);
+	gridWorld.coordToStates();
+	gridWorld.createRewardTable();
+
+	//create agent
+	agent doubleOseven; 
+	doubleOseven.init();
+	doubleOseven.placeAgent(gridWorld.xcoord, gridWorld.ycoord, gridWorld.xmax, gridWorld.ymax);
+	doubleOseven.initQ(gridWorld.states);
+
+	cout << "Agent's state: " << doubleOseven.state << endl;
+	cout << "Goal State: " << gridWorld.goalState << endl;
+	system("pause");
+
+	while (doubleOseven.state != gridWorld.goalState)
+	{
+		doubleOseven.moveAgent(gridWorld);
+	}
+
+	cout << "Goal was at: " << gridWorld.goalPosX << ", " << gridWorld.goalPosY << endl;
+	cout << "Agent ended at" << doubleOseven.xpos << ", " << doubleOseven.ypos << endl;
+	cout << "Number of moves: " << doubleOseven.numMoves << endl;
 
 	system("pause");
     return 0;
